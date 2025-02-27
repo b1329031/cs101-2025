c28233463c4bb63caeda383c5986e6159a450057
b1329031 <bobo20060708@gmail.com>
2025-02-27 14:02:25 +0800

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,18 @@
+
+C Online Compiler
+
+Programiz PRO ❯
+main.c
+
+
+Share
+
+ Run 
+    printf("first %d + last %d = %d", first, last, first+last);
+
+
+Output
+ Clear 
+=== Code Execution Successful ===
+
+
