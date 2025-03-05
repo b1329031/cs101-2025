d827e276921698f3c478ecdda4530344d22291ae
b1329031 <bobo20060708@gmail.com>
2025-03-05 09:47:41 +0800

    Update main3.c

--- main3.c
+++ main3.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    unsigned char i = 200;
-    unsigned char j = 220;
-    unsigned char k = 0;
-    k = ((j - i) / 2) + i;
-    printf("(%d + %d)/2 = %d\n", i, j, k);
+void binary_to_hex(const char *binary){
+    int decimal = 0;
+
+    for(int i = 0; i < 8; i ++){
+        decimal = decimal* 2 + (binary[i] - '0');
+    }
+    printf("%X\n", decimal);
+}
+
+int main(){
+    char a[] = "10001111";
+    binary_to_hex(a);
     return 0;
+
 }
