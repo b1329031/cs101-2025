3201e821d149aad98a902ed7f6706310b953a6ea
b1329031 <bobo20060708@gmail.com>
2025-03-25 23:38:26 +0800

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,18 +1,14 @@
+#include<stdio.h>
 
-C Online Compiler
-
-Programiz PRO ❯
-main.c
-
-
-Share
-
- Run 
-    printf("first %d + last %d = %d", first, last, first+last);
-
-
-Output
- Clear 
-=== Code Execution Successful ===
-
-
+int main(){
+    int i = 123345;
+    int first;
+    int last = first = i % 10;
+    
+    while(i >= 10){
+        i /= 10;
+        first = i % 10;
+    }
+    printf("first %d =+ last %d = %d", first, last, last+first);
+    return 0;
+}
